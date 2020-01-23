752bf2494928de1edc2f4b6a3b77224fd62a2f95
Alec Mather <aymather@gmail.com>
2020-01-22 18:08:31 -0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import Dashboard from './lib/wrapper/Dashboard';
+import Body from './lib/body/Body';
+import LoggedOut from './lib/wrapper/LoggedOut';
+import Form from './lib/form/Form';
+import FormBtn from './lib/form/FormBtn';
+import FormFooter from './lib/form/FormFooter';
+import FormFooterLink from './lib/form/FormFooterLink';
+import FormHeader from './lib/form/FormHeader';
+import FormHeaderLogo from './lib/form/FormHeaderLogo';
+import FormHeaderTitle from './lib/form/FormHeaderTitle';
+import FormInput from './lib/form/FormInput';
+import Navbar from './lib/navbar/Navbar';
+import NavbarItem from './lib/navbar/NavbarItem';
+import NavbarSection from './lib/navbar/NavbarSection';
+import Sidebar from './lib/sidebar/Sidebar';
+import SidebarBody from './lib/sidebar/SidebarBody';
+import SidebarBodySection from './lib/sidebar/SidebarBodySection';
+import SidebarBodySectionItem from './lib/sidebar/SidebarBodySectionItem';
+import SidebarBodySectionTitle from './lib/sidebar/SidebarBodySectionTitle';
+import SidebarHeader from './lib/sidebar/SidebarHeader';
+import Tabs from './lib/tabs/Tabs';
+import Tab from './lib/tabs/Tab';
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+export {
+	LoggedOut,
+	Dashboard,
+	Body,
+	Form,
+	FormBtn,
+	FormFooter,
+	FormFooterLink,
+	FormHeader,
+	FormHeaderLogo,
+	FormHeaderTitle,
+	FormInput,
+	Navbar,
+	NavbarItem,
+	NavbarSection,
+	Sidebar,
+    SidebarBody,
+    SidebarBodySection,
+    SidebarBodySectionItem,
+    SidebarBodySectionTitle,
+	SidebarHeader,
+	Tabs,
+	Tab
 }
